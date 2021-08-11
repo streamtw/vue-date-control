382d75b4848c5e794249637bb8fba2a159cb2925
Stream <g0110280@gmail.com>
2021-08-11 09:33:55 +0800

    feat: add date control component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template lang="pug">
-  #app
-    #nav
-      router-link(to="/") Home
-      | &nbsp;|&nbsp;
-      router-link(to="/about") About
-    router-view
+#app
+  DateControl(ref="date" v-model="dateOfComponent" @input="show")
+  .formatted-date {{ dateOfComponent.year }} 年 {{ dateOfComponent.month }} 月 {{ dateOfComponent.day }} 日 ({{ dateOfComponent.weekday }})
+  .date-picker-wrapper
+    .date-picker
+      button.date-picker__button.date-picker__button--add(@click="$refs.date.add('year')")
+      .date-picker__value {{ dateOfComponent.year }}
+      button.date-picker__button.date-picker__button--subtract(@click="$refs.date.subtract('year')")
+    .date-picker
+      button.date-picker__button.date-picker__button--add(@click="$refs.date.add('month')")
+      .date-picker__value {{ dateOfComponent.month }}
+      button.date-picker__button.date-picker__button--subtract(@click="$refs.date.subtract('month')")
+    .date-picker
+      button.date-picker__button.date-picker__button--add(@click="$refs.date.add('day')")
+      .date-picker__value {{ dateOfComponent.day }}
+      button.date-picker__button.date-picker__button--subtract(@click="$refs.date.subtract('day')")
 </template>
 
+<script>
+import DateControl from '@/components/DateControl'
+
+export default {
+  components: { DateControl },
+  data () {
+    return {
+      dateOfComponent: {},
+    }
+  },
+  methods: {
+    show () {
+      console.log(this.dateOfComponent)
+    },
+  },
+}
+</script>
+
 <style lang="sass">
+*
+  box-sizing: border-box
+
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
+  width: 100vw
+  height: 100vh
+  padding: 100px
 
-#nav
-  padding: 30px
+  .formatted-date
+    text-align: center
+    font-size: 1.5rem
 
-  a
-    font-weight: bold
-    color: #2c3e50
+  .date-picker-wrapper
+    display: flex
+    align-items: center
+    justify-content: center
+    margin-top: 32px
 
-    &.router-link-exact-active
-      color: #42b983
+  .date-picker
+    text-align: center
+
+    > * + *
+      margin-top: 8px
+
+    .date-picker__button
+      background: none
+      border-right: 24px solid transparent
+      border-left: 24px solid transparent
+      height: 0
+      padding: 0
+
+      &.date-picker__button--add
+        border-bottom: 24px solid black
+        border-top: none
+
+      &.date-picker__button--subtract
+        border-top: 24px solid black
+        border-bottom: none
+
+  .date-picker + .date-picker
+    margin-left: 32px
+
 </style>
---
+++ src/components/DateControl.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  .date-control
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'DateControl',
+  props: {
+    msg: String,
+    value: Object,
+  },
+  data () {
+    return {
+      date: moment(),
+    }
+  },
+  mounted () {
+    moment.locale('zh-TW')
+    this.$_fireEvent()
+  },
+  methods: {
+    add (type) {
+      this.date.add(1, type)
+      this.$_fireEvent()
+    },
+    subtract (type) {
+      this.date.subtract(1, type)
+      this.$_fireEvent()
+    },
+    $_getWeekdayLabel (weekdayIndex) {
+      return moment.weekdaysMin()[weekdayIndex]
+    },
+    $_fireEvent () {
+      this.$emit('input', {
+        year: this.date.year(),
+        month: this.date.month() + 1,
+        day: this.date.date(),
+        weekday: this.$_getWeekdayLabel(this.date.weekday()),
+      })
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+//
+</style>
